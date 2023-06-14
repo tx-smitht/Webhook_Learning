58ba66379a140ff18335da5933d3add29383e73f
Tom Smith <tx.smiththomas@gmail.com>
2023-06-14 15:08:58 -0400

    adding comments

--- python_webhook.py
+++ python_webhook.py
@@ -1,12 +1,15 @@
 import requests
 import json
 
+# Webhook URL from webhook.site .
 webhook_url ='https://webhook.site/55fb8ec1-a614-4d40-bfb9-30e287728770'
 
+# Create the fake data
 data = { 'name': 'Tom\'s new webhook',
         'Current status': 'hungry',
         'Current favorite animal': 'golden eagle'}
 
+# Make a post request
 r = requests.post(
     webhook_url,
     data=json.dumps(data),
